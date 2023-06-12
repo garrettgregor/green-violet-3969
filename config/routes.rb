e99237963bc08c42d0a91d5c10efac6b0b5e40f9
Garrett Gregor <garrett.gregor@gmail.com>
2023-06-12 11:58:21 -0600

    Refactor: update resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,26 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :flights, only: [:index] do
+  # resources :flights, only: [:index] do
+  # # namespace :flights, only: [:index] do
+  #   resources :passengers, only: [:destroy]#, controller: "flights/passengers"
+  # end
+  # #should be to flights/passengers
+  # get "flights/:id/passengers/:id", to: "passengers#destroy"
+
+  # resources :airlines, only: [:show]
+
+  # resources :airlines
+  # resources :flights do
+  #   resources :passengers
+  # end
+  # resources :passengers
   # namespace :flights, only: [:index] do
-    resources :passengers, only: [:destroy]#, controller: "flights/passengers"
-  end
   #should be to flights/passengers
-  get "flights/:id/passengers/:id", to: "passengers#destroy"
-
+  # get "flights/:id/passengers/:id", to: "passengers#destroy"
+  resources :flights, only: [:index] do
+    resources :passengers, only: [:show, :destroy]#, controller: "flights/passengers"
+  end
+  
   resources :airlines, only: [:show]
-
 end
